ec9645dc7e3a49647ad38dd65a70ac2c1bb6c6b4
yashs <yashsathvara2003@gmail.com>
2024-07-17 12:01:19 +0530

    commmit

---
+++ app/src/main/java/com/example/swara/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.swara;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000); // 3000 milliseconds = 3 seconds
+
+        // Automatically navigate to SecondActivity
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(intent);
+
+        // Optional: Finish MainActivity if you don't want the user to return to it
+        finish();
+
+    }
+}
